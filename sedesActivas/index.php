cc10c7c6c2cab778913df9c62db26672f256d39b
unknown <ivancas84@gmail.com>
2019-07-13 00:07:24 -0300

    adaptar template de sedes activas

--- sedesActivas/index.php
+++ sedesActivas/index.php
@@ -7,14 +7,18 @@ require_once("function/array_combine_key.php");
 require_once("function/array_combine_keys.php");
 require_once("function/array_unique_key.php");
 require_once("function/array_group_value.php");
+require_once("function/fecha_anios.php");
+require_once("function/fecha_semestres.php");
+require_once("function/dependencias.php");
+require_once("function/clasificaciones.php");
 
 
 $fechaAnio = isset($_GET["fecha_anio"]) ? $_GET["fecha_anio"] : date("Y");
-$fechaSemestre = isset($_GET["fecha_semestre"]) ? $_GET["fecha_semestre"] : (date("m") < 7) ? 1 : 2;
+if (isset($_GET["fecha_semestre"])) $fechaSemestre = $_GET["fecha_semestre"];
+else $fechaSemestre = (date("m") < 7) ? 1 : 2;
 $clasificacion = isset($_GET["clasificacion"]) ? $_GET["clasificacion"] : "Fines";
 $dependencia_ = isset($_GET["dependencia"]) ? $_GET["dependencia"] : "Todos";
 $dependencia = ($dependencia_ == "Todos") ?  $_SESSION["dependencia"] : $dependencia_;
-require_once("_periodoClasificacion/options.php");
 
 $title = "Sedes Activas";
 
@@ -26,12 +30,16 @@ $render->setAdvanced([
     ["dependencia", "=", $dependencia],
     ["_clasificacion_nombre", "=", $clasificacion]
 ]);
+$sql = SedeSql::getInstance()->_subSql($render);
+$idSedes = array_column(Dba::fetchAll($sql), "id");
+
+
+$render = new Render();
+$render->setAdvanced(["id","=",$idSedes]);
 $render->setOrder(["numero" => "ASC"]);
 
 $sql = SedeSqlo::getInstance()->all($render);
 $sedes = Dba::fetchAll($sql);
-
-$idSedes = array_unique_key($sedes, "id");
 
 $render = new Render();
 $render->setAdvanced([

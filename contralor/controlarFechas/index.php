33eec77a1971fbf3a87c5ce03d7120f49002b156
ivan <ivancas84@gmail.com>
2019-10-07 15:18:36 -0300

    agrupar todo el contralor

---
+++ contralor/controlarFechas/index.php
@@ -0,0 +1,19 @@
+<?php
+require_once("../../config/config.php");
+require_once("function/fecha_anios.php");
+require_once("function/fecha_semestres.php");
+require_once("function/clasificaciones.php");
+require_once("function/dependencias.php");
+
+$content = "contralor/controlarFechaFin/formulario.html";
+
+$dependencia_ = isset($_GET["dependencia"]) ? $_GET["dependencia"] : "Todos";
+$fechaAnio = isset($_GET["fecha_anio"]) ? $_GET["fecha_anio"] : date("Y");
+if(isset($_GET["fecha_semestre"])) $fechaSemestre = $_GET["fecha_semestre"];
+else  $fechaSemestre = (date("m") < 7) ? 1 : 2;
+$clasificacion = isset($_GET["clasificacion"]) ? $_GET["clasificacion"] : "Fines";
+$dependencia = ($dependencia_ == "Todos") ?  $_SESSION["dependencia"] : $dependencia_;
+$fechaEntradaContralor = isset($_GET["fecha_entrada_contralor"]) ? $_GET["fecha_entrada_contralor"] : false; 
+
+
+require_once("index/menu.html");
---
+++ contralor/controlarFechas/informe.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Contralor definido en base a los siguientes requerimientos (establecidos por la DEA en marzo de 2019)
+ */
+
+require_once("../config/config.php");
+require_once("class/model/Data.php");
+require_once("class/model/Values.php");
+
+$dependencia_ = isset($_GET["dependencia"]) ? $_GET["dependencia"] : "Todos";
+$fechaAnio = isset($_GET["fecha_anio"]) ? $_GET["fecha_anio"] : date("Y");
+if(isset($_GET["fecha_semestre"])) $fechaSemestre = $_GET["fecha_semestre"];
+else  $fechaSemestre = (date("m") < 7) ? 1 : 2;
+$clasificacion = isset($_GET["clasificacion"]) ? $_GET["clasificacion"] : "Fines";
+$dependencia = ($dependencia_ == "Todos") ?  $_SESSION["dependencia"] : $dependencia_;
+if(empty($fechaInicio = $_GET["fecha_inicio"])) die("Fecha inicio no definida");
+if(empty($fechaFin = $_GET["fecha_fin"])) die("Fecha fin no definida");
+
+$sql = Data::contralor($fechaAnio, $fechaSemestre, $clasificacion, $fechaEntradaContralor);
+$rows = Dba::fetchAll($sql);
+
+require_once("./informe.html");
